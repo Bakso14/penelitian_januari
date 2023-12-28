8c7759c706000e77e55c2cd79036ea49d56129c8
Bakso14 <70924870+Bakso14@users.noreply.github.com>
2023-12-28 19:51:16 +0700

    Create tes_opencv.py
    tes_camera

---
+++ tes_opencv.py
@@ -0,0 +1,22 @@
+import cv2
+
+cap = cv2.VideoCapture(0)
+
+while(1):
+    _,frame = cap.read()
+    scale_percent = 30 # percent of original size
+    width = int(frame.shape[1] * scale_percent / 100)
+    height = int(frame.shape[0] * scale_percent / 100)
+    dim = (width, height)
+
+    # resize image
+    frame = cv2.resize(frame, dim, interpolation = cv2.INTER_AREA) 
+
+    cv2.imshow('frame',frame)
+    
+    k = cv2.waitKey(5) & 0xFF
+    if k == 27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
